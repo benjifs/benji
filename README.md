d0a0a22ef558f44ebfe169df4aaf9d28980a7562
Benji Encalada Mora <benjifs@users.noreply.github.com>
2021-12-17 17:05:02 -0600

    feat: update webmentions to use microformats instead of RSS or JSONFeed
    minor cleanup too

--- README.md
+++ README.md
@@ -1,29 +1,21 @@
 # benji.dog
+[![GitHub Status](https://github.com/benjifs/benji/actions/workflows/eleventy_build.yml/badge.svg)](https://github.com/benjifs/benji/actions)
+
 [![Netlify Status](https://api.netlify.com/api/v1/badges/8adf49ae-cb8e-407b-bf3b-1b3c8a6af409/deploy-status)](https://app.netlify.com/sites/benji-dog/deploys)
 
 My [personal website](https://benji.dog) built with [11ty](https://11ty.dev).
 
-The [production](https://benji.dog) version of this website runs on a separate server. This repo is setup to automatically deploy with [Netlify](https://netlify.com) whenever there is a `push` to the [micropub-posts](https://github.com/benjifs/benji/tree/micropub-posts) branch.
-
-## Setup
-
-```bash
-npm install
-```
-
-## Development
-
-```bash
-npm run start
-```
+The [production](https://benji.dog) version of this website runs on a separate server. On a push to the `main` branch, a workflow runs and builds the website and after it completes running, it will deploy the changes to my server. This repo is also setup to deploy to [GitHub Pages](https://pages.github.com/) and [Netlify](https://netlify.com) but those are mainly there for reference purpose.
 
 ## Build
 
-```bash
-npm run build
-```
+To build, run `npm install` and `npm run build`. If doing active development, you can also run `npm run start` to watch for changes and rebuild as needed.
 
 ## Deploy
+
+The easiest way to deploy this website is to upload the `_site` directory that shows up after a successful built. You can also fork this repo and make the required environment variable changes (see [.env.example](https://github.com/benjifs/benji/blob/main/.env.example)) and it should build once you enable [GitHub Pages](https://pages.github.com/).
+
+An easier approach is to use the "Netlify Deploy" button from below which should run through the process of cloning and setting up everything for you.
+
 [![Netlify Deploy](https://www.netlify.com/img/deploy/button.svg)](https://app.netlify.com/start/deploy?repository=https://github.com/benjifs/benji)
 
-Currently setup to deploy to Netlify, GH Pages, and upload to my server. Mostly just as a way to compare and to have a sample of a way to do this. Will stick to one approach in the future.
--- webmentions.js
+++ webmentions.js
@@ -1,45 +1,91 @@
 
 const fs = require('fs')
 const got = require('got')
+const Webmention = require('@remy/webmention')
+const mf = require('microformat-node')
 
 require('dotenv').config()
 
-const parse = (json = '') => JSON.parse(json)
+const {
+	URL, // baseURL to resolve relative URLs for MF parser
+	FEED_URL, // URL of feed page. Otherwise will try to default or discover from MF
+	SAVED_FEED = './feed.html', // Filename of previously saved feed
+} = process.env;
+const DEBUG = process.env.DEBUG === 'true'
+const wm = new Webmention({ limit: 1, send: !DEBUG })
 
-const getLatest = feed => {
-	if (feed && feed.items && feed.items.length) {
-		return feed.items[0]
+const sendWebmention = async (url) => {
+	await new Promise((resolve, reject) => {
+		wm.on('error', e => { reject(e) })
+		wm.on('sent', res => { console.log(`[SENT] ${res.source} to ${res.endpoint.url}`) })
+		wm.on('end', () => { resolve() })
+		wm.fetch(url)
+	})
+}
+
+const parse = {
+	getFeedURL: parsed => {
+		if (parsed && parsed.rels && parsed.rels.feed) {
+			return Array.isArray(parsed.rels.feed) ? parsed.rels.feed[0] : parsed.rels.feed
+		}
+	},
+	getFeed: parsed => {
+		if (parsed && parsed.items && parsed.items.length &&
+			parsed.items[0].children && parsed.items[0].children.length) {
+			return parsed.items[0].children
+		}
+	},
+	getItemID: item => {
+		if (item && item.properties && item.properties.url) {
+			return Array.isArray(item.properties.url) ? item.properties.url[0] : item.properties.url
+		}
 	}
 }
 
 const checkWebmentions = async () => {
-	const readFeed = fs.readFileSync('./feed.json')
-	const feedOld = parse(readFeed)
-	const feedNew = await got.get(`${process.env.URL}/feed.json`).json()
+	const oldFeed = fs.readFileSync(SAVED_FEED, 'utf-8')
+	const mfOld = mf.get({
+		html: oldFeed,
+		baseUrl: URL
+	})
 
-	const latestOld = getLatest(feedOld)
-	const latestNew = getLatest(feedNew)
+	const newFeed = await got.get(FEED_URL || parse.getFeedURL(mfOld) || `${URL}/feed`).text()
+	const mfNew = mf.get({
+		html: newFeed,
+		baseUrl: URL
+	})
 
-	if (latestNew && latestOld) {
-		for (let item of feedNew.items) {
-			if (item.id == latestOld.id) { // Stop checking if `id` matches previous latest `id`
+	const mfFeedOld = parse.getFeed(mfOld)
+	const mfFeedNew = parse.getFeed(mfNew)
+
+	if (mfFeedOld && mfFeedNew) {
+		const lastChecked = parse.getItemID(mfFeedOld[0])
+		DEBUG && console.log('[INFO] Last checked item:', lastChecked)
+		for (let item of mfFeedNew) {
+			const currentItemID = parse.getItemID(item)
+			if (currentItemID == lastChecked) {
 				break
 			}
-			if (!item.title || item.title.indexOf('Replying') < 0) { // Is a reply?
-				continue
-			}
-			const res = await got.post(`https://webmention.app/check/?url=${item.id}`).json()
-			if (!res || res.error) {
-				console.error({
-					'error': item.id,
-					'message': res.message || 'An error occurred'
-				})
+			const shouldWebmention = Object.keys(item.properties)
+				.filter(item => [
+					'like-of', 'u-like-of',
+					'in-reply-to', 'u-in-reply-to',
+					'repost-of', 'u-repost-of',
+					'bookmark-of', 'u-bookmark-of'
+				].includes(item))
+			if (shouldWebmention && shouldWebmention.length) {
+				DEBUG && console.log('[SEND]', currentItemID)
+				try {
+					await sendWebmention(currentItemID)
+				} catch(err) {
+					console.error('[ERROR]', currentItemID, err || 'An error occurred')
+				}
 			} else {
-				console.log(`Webmention sent for ${item.id}`)
+				DEBUG && console.log('[SKIP]', currentItemID)
 			}
 		}
 	} else {
-		console.log('NOTHING TO SEND')
+		DEBUG && console.log('NOTHING TO SEND')
 		process.exit(1)
 	}
 }
