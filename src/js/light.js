349abe3847873e5813c26d24d23e05e5f17b4267
Benji Encalada Mora <benjifs@users.noreply.github.com>
2023-10-30 15:45:09 -0500

    style: "spring" cleaning

--- src/js/light.js
+++ src/js/light.js
@@ -1,25 +1,17 @@
-
 function light_switch() {
-	const $html = document.documentElement;
-	setTheme($html.classList.contains('light') ? 'dark' : 'light');
+	const $html = document.documentElement
+	setTheme($html.dataset.theme == 'light' ? 'dark' : 'light')
 }
 
 function setTheme(mode) {
-	const $html = document.documentElement;
-	const $switch = document.getElementById('light-switch');
-
-	if (mode === 'light') {
-		$html.classList.add('light');
-		$switch.src = '/assets/phosphor/moon.svg'
-	} else {
-		$html.classList.remove('light');
-		$switch.src = '/assets/phosphor/sun.svg'
-	}
-	$switch.setAttribute('aria-label', 'Switch to ' + (mode === 'light' ? 'dark' : 'light') + ' mode');
-
-	localStorage.setItem('theme', mode);
+	const $html = document.documentElement
+	const $switch = document.getElementById('light-switch')
+	$html.dataset.theme = mode
+	$switch.src = '/assets/phosphor/' + (mode === 'light' ? 'moon' : 'sun') + '.svg'
+	$switch.setAttribute('aria-label', 'Switch to ' + (mode === 'light' ? 'dark' : 'light') + ' mode')
+	localStorage.setItem('theme', mode)
 }
 
-document.addEventListener('DOMContentLoaded', e => {
-	setTheme(localStorage.getItem('theme'));
-});
+document.addEventListener('DOMContentLoaded', () => {
+	setTheme(localStorage.getItem('theme'))
+})
--- src/js/oce.js
+++ src/js/oce.js
@@ -1,9 +1,8 @@
-
-var $el = document.querySelector('[data-mailto]');
+var $el = document.querySelector('[data-mailto]')
 if ($el && $el.dataset.mailto) {
 	// Sort of obfuscating email
 	// If javascript is disabled, don't show email
 	// Decodes a base64 string and reverses it to get the actual email
-	$el.href = 'mailto:' + atob($el.dataset.mailto).split('').reverse().join('');
-	$el.style.display = 'initial';
+	$el.href = 'mailto:' + atob($el.dataset.mailto).split('').reverse().join('')
+	$el.style.display = ''
 }
