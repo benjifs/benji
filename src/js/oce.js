6e76f5e51048ac830df1870b4be3d9a0d20f79de
Benji Encalada Mora <benjifs@users.noreply.github.com>
2024-02-03 00:11:21 -0600

    feat: new home screen

--- src/js/oce.js
+++ src/js/oce.js
@@ -1,8 +1,13 @@
-var $el = document.querySelector('[data-mailto]')
-if ($el && $el.dataset.mailto) {
-	// Sort of obfuscating email
-	// If javascript is disabled, don't show email
-	// Decodes a base64 string and reverses it to get the actual email
-	$el.href = 'mailto:' + atob($el.dataset.mailto).split('').reverse().join('')
-	$el.style.display = ''
+const $els = document.querySelectorAll('[data-mailto]')
+if ($els && $els.length) {
+	for (const i in $els) {
+		const $el = $els[i]
+		if ($el.dataset && $el.dataset.mailto) {
+			// Sort of obfuscating email
+			// If javascript is disabled, don't show email
+			// Decodes a base64 string and reverses it to get the actual email
+			$el.href = 'mailto:' + atob($el.dataset.mailto).split('').reverse().join('')
+			$el.style.display = ''
+		}
+	}
 }
