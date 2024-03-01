ab1d564399faad44bd262b927636152df21eb491
Benji Encalada Mora <benjifs@users.noreply.github.com>
2024-03-01 00:42:27 -0600

    fix: still render note

--- src/content/content.11tydata.js
+++ src/content/content.11tydata.js
@@ -5,7 +5,6 @@ module.exports = {
 		"layout": data => data.deleted ? "layouts/deleted.njk" : data.layout,
 		"permalink": data => {
 			if (data.draft) return false
-			if (['unlisted', 'private'].includes(data.visibility)) return false
 			// Content is placed in a subdir for convenience but I don't want this to be part of the URL
 			// Use permalink or remove /content/ from final permalink
 			return (data.permalink || data.page.filePathStem.replace('/content/', '')) + '/'
