b5a82ee6c35991c21c68c3a7d0661e16c5f14617
Benji Encalada Mora <benjifs@users.noreply.github.com>
2024-04-09 00:26:05 -0500

    style: prep for css-naked-day

--- src/js/light.js
+++ src/js/light.js
@@ -1,11 +1,13 @@
 const $html = document.documentElement
+const prefersDark = () => window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
 document.addEventListener('DOMContentLoaded', () => {
 	const theme = localStorage.getItem('theme')
 	if (theme) $html.dataset.theme = theme
 })
 
 document.getElementById('theme-switch').addEventListener('change', e => {
-	const isDark = $html.dataset.theme == 'dark' ||
-		(!$html.dataset.theme && window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches)
-	localStorage.setItem('theme', isDark && e.currentTarget.checked ? 'light' : 'dark')
+	const isDark = $html.dataset.theme == 'dark' || (!$html.dataset.theme && prefersDark())
+	const theme = isDark && e.currentTarget.checked ? 'light' : 'dark'
+	localStorage.setItem('theme', theme)
+	if (theme) $html.dataset.theme = theme
 })
