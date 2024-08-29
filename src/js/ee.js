fd5e38f4c4736e67e0c27c2ac062f93121f530d2
Benji Encalada Mora <benjifs@users.noreply.github.com>
2024-08-29 12:56:37 -0500

    refactor: cleanup

--- src/js/ee.js
+++ src/js/ee.js
@@ -1,28 +1,23 @@
 (function () {
-	var w = window,
+	const w = window,
 		d = document,
 		e = d.documentElement,
 		b = d.getElementsByTagName('body')[0],
 		x = w.innerWidth || e.clientWidth || b.clientWidth,
-		y = w.innerHeight || e.clientHeight || b.clientHeight;
+		y = w.innerHeight || e.clientHeight || b.clientHeight
 
-	var random = function (n) {
-		return Math.floor(Math.random() * n);
-	}
+	const random = n => Math.floor(Math.random() * n)
 
 	if (random(10) == 0) {
-		var dot = document.createElement('div');
-		dot.className = 'dot';
-
-		dot.style.top = random(y) + 'px';
-		dot.style.left = random(x) + 'px';
-
-		b.appendChild(dot);
-
-		dot.onclick = function () {
-			d.getElementsByTagName('html')[0].className += ' swag';
-			dot.remove();
-			w.addEgg('red');
+		const dot = document.createElement('div')
+		dot.className = 'dot'
+		dot.style.top = `${random(y)}px`
+		dot.style.left = `${random(x)}px`
+		b.appendChild(dot)
+		dot.onclick = () => {
+			d.getElementsByTagName('html')[0].className += ' swag'
+			dot.remove()
+			w.addEgg('red')
 		}
 	}
 
@@ -31,7 +26,7 @@
 							'                 \\_`_/ |/\n' +
 							'                <| ^ |>|   type yahaha()\n' +
 							'                 | _ | |      to claim 🥚\n' +
-							'                  " "');
+							'                  " "')
 
 	const ee = '__ee'
 	w.eggs = () => JSON.parse(localStorage.getItem(ee) || '[]')
