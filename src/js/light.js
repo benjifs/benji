8b8b3139246488eebf194b06e0039b8b19515d20
Benji Encalada Mora <benjifs@users.noreply.github.com>
2024-09-28 00:21:09 -0500

    style: fixing theme toggle

--- src/js/light.js
+++ src/js/light.js
@@ -1,13 +1,13 @@
 const $html = document.documentElement
 const prefersDark = () => window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches
+let startedDark
 document.addEventListener('DOMContentLoaded', () => {
 	const theme = localStorage.getItem('theme')
 	if (theme) $html.dataset.theme = theme
+	startedDark = $html.dataset.theme == 'dark' || (!$html.dataset.theme && prefersDark())
 })
-
 document.getElementById('theme-switch').addEventListener('change', e => {
-	const isDark = $html.dataset.theme == 'dark' || (!$html.dataset.theme && prefersDark())
-	const theme = isDark && e.currentTarget.checked ? 'light' : 'dark'
+	const theme = startedDark ^ e.currentTarget.checked ? 'dark' : 'light'
 	localStorage.setItem('theme', theme)
 	if (theme) $html.dataset.theme = theme
 })
